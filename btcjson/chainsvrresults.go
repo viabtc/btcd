b6f225f27d49cbf8a83cdee7a825bac8252d796a
binwenbang <binwenbang@viabtc.com>
2023-04-18 18:19:38 +0800

    增加ScriptPubKeyResult的Address字段

--- btcjson/chainsvrresults.go
+++ btcjson/chainsvrresults.go
@@ -432,6 +432,7 @@ type ScriptPubKeyResult struct {
 	ReqSigs   int32    `json:"reqSigs,omitempty"`
 	Type      string   `json:"type"`
 	Addresses []string `json:"addresses,omitempty"`
+	Address   string   `json:"address"`
 }
 
 // GetTxOutResult models the data from the gettxout command.
